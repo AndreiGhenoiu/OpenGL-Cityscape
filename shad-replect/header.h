337daf8649412debd84dc3e65acf08185b7d0acd
Andrei Ghenoiu <andrei_stefang@yahoo.com>
2012-07-13 15:35:21 -0400

    files

---
+++ shad-replect/header.h
@@ -0,0 +1,309 @@
+
+
+void FreeTexture( GLuint texture )
+{
+  glDeleteTextures( 1, &texture );
+}
+//loading the texture
+GLuint LoadTexture( const char * filename, int width, int height )
+{
+    GLuint texture;
+    unsigned char * data;
+    FILE * file;
+
+    //The following code will read in our RAW file
+    file = fopen( filename, "rb" );
+    if ( file == NULL ) return 0;
+    data = (unsigned char *)malloc( width * height * 3 );
+    fread( data, width * height * 3, 1, file );
+    fclose( file );
+
+    glGenTextures( 1, &texture ); //generate the texture with the loaded data
+    glBindTexture( GL_TEXTURE_2D, texture ); //bind the texture to it’s array
+    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE ); //set texture environment parameters
+
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+
+    //Here we are setting the parameter to repeat the texture instead of clamping the texture
+    //to the edge of our shape.
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    //Generate the texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    free( data ); //free the texture
+    return texture; //return whether it was successfull
+}
+
+void createStreet(int NWpointX, int NWpointZ, int SEpointX, int SEpointZ,GLuint texture){
+
+     glBindTexture( GL_TEXTURE_2D, texture );
+    glPushMatrix();
+
+    glBegin(GL_QUADS);
+
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,.03,NWpointZ);
+    glTexCoord2f(10.0,0.0);
+    glVertex3f(SEpointX,.03,NWpointZ);
+     glTexCoord2f(1.0,1.0);
+    glVertex3f(SEpointX,.03,SEpointZ);
+    glTexCoord2f(0.0,1.0);
+    glVertex3f(NWpointX,.03,SEpointZ);
+
+    glEnd();
+    glPopMatrix();
+
+}
+
+void createCar(float NWpointX, float NWpointZ, float SEpointX, float SEpointZ, float height){
+    //front texture
+    GLuint front = LoadTexture( "car-front.raw", 256, 256 );
+    //rear texture
+    GLuint rear = LoadTexture( "car-rear.raw", 256, 256 );
+    //profile texture
+    GLuint profile = LoadTexture( "car-profile.raw", 256, 256 );
+
+    glPushMatrix();
+    glScalef(1,-1,1);
+    glTranslatef(0,-height,0);
+
+    glBindTexture( GL_TEXTURE_2D, rear );
+    glBegin(GL_QUADS);
+    glNormal3f(0,0,1);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,SEpointZ);
+    glTexCoord2f(1.0,0.0);
+    glVertex3f(SEpointX,0,SEpointZ);
+    glTexCoord2f(1.0,1.0);
+    glVertex3f(SEpointX,height,SEpointZ);
+    glTexCoord2f(0.0,1.0);
+    glVertex3f(NWpointX,height,SEpointZ);
+    glEnd();
+
+    glBindTexture( GL_TEXTURE_2D, profile );
+    glBegin(GL_QUADS);
+    glNormal3f(-1,0,0);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,NWpointZ);
+    glTexCoord2f(1.0,0.0);
+    glVertex3f(NWpointX,0,SEpointZ);
+    glTexCoord2f(1.0,1.0);
+    glVertex3f(NWpointX,height,SEpointZ);
+    glTexCoord2f(0.0,1.0);
+    glVertex3f(NWpointX,height,NWpointZ);
+    glEnd();
+
+    glBindTexture( GL_TEXTURE_2D, front );
+    glBegin(GL_QUADS);
+    glColor3f(1,0,0);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,NWpointZ);
+    glTexCoord2f(1.0,0.0);
+    glVertex3f(SEpointX,0,NWpointZ);
+    glTexCoord2f(1.0,1.0);
+    glVertex3f(SEpointX,height,NWpointZ);
+    glTexCoord2f(0.0,1.0);
+    glVertex3f(NWpointX,height,NWpointZ);
+    glEnd();
+
+    glBindTexture( GL_TEXTURE_2D, profile );
+    glBegin(GL_QUADS);
+    glNormal3f(1,0,0);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(SEpointX,0,NWpointZ);
+    glTexCoord2f(1.0,0.0);
+    glVertex3f(SEpointX,0,SEpointZ);
+    glTexCoord2f(1.0,1.0);
+    glVertex3f(SEpointX,height,SEpointZ);
+    glTexCoord2f(0.0,1.0);
+    glVertex3f(SEpointX,height,NWpointZ);
+    glEnd();
+
+    glBindTexture( GL_TEXTURE_2D, profile );
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,-height/100,NWpointZ);
+    glTexCoord2f(10.0,0.0);
+    glVertex3f(SEpointX,-height/100,NWpointZ);
+     glTexCoord2f(10.0,10.0);
+    glVertex3f(SEpointX,-height/100,SEpointZ);
+    glTexCoord2f(0.0,10.0);
+    glVertex3f(NWpointX,-height/100,SEpointZ);
+    glEnd();
+
+    glPopMatrix();
+
+    FreeTexture(front);
+    FreeTexture(rear);
+    FreeTexture(profile);
+}
+void createBuilding(float NWpointX, float NWpointZ, float SEpointX, float SEpointZ, float height, GLuint texture){
+
+
+    glBindTexture( GL_TEXTURE_2D, texture );
+    glPushMatrix();
+
+    glBegin(GL_QUADS);
+
+    glNormal3f(0,0,1);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,SEpointZ);
+    glTexCoord2f(4.0,0.0);
+    glVertex3f(SEpointX,0,SEpointZ);
+    glTexCoord2f(4.0,4.0);
+    glVertex3f(SEpointX,height,SEpointZ);
+    glTexCoord2f(0.0,4.0);
+    glVertex3f(NWpointX,height,SEpointZ);
+
+    glNormal3f(-1,0,0);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,NWpointZ);
+    glTexCoord2f(4.0,0.0);
+    glVertex3f(NWpointX,0,SEpointZ);
+    glTexCoord2f(4.0,4.0);
+    glVertex3f(NWpointX,height,SEpointZ);
+    glTexCoord2f(0.0,4.0);
+    glVertex3f(NWpointX,height,NWpointZ);
+
+    glColor3f(1,0,0);
+     glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,0,NWpointZ);
+    glTexCoord2f(4.0,0.0);
+    glVertex3f(SEpointX,0,NWpointZ);
+    glTexCoord2f(4.0,4.0);
+    glVertex3f(SEpointX,height,NWpointZ);
+    glTexCoord2f(0.0,4.0);
+    glVertex3f(NWpointX,height,NWpointZ);
+
+    glNormal3f(1,0,0);
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(SEpointX,0,NWpointZ);
+    glTexCoord2f(4.0,0.0);
+    glVertex3f(SEpointX,0,SEpointZ);
+    glTexCoord2f(4.0,4.0);
+    glVertex3f(SEpointX,height,SEpointZ);
+    glTexCoord2f(0.0,4.0);
+    glVertex3f(SEpointX,height,NWpointZ);
+
+    glTexCoord2f(0.0,0.0);
+    glVertex3f(NWpointX,height,NWpointZ);
+    glTexCoord2f(100.0,0.0);
+    glVertex3f(SEpointX,height,NWpointZ);
+     glTexCoord2f(100.0,100.0);
+    glVertex3f(SEpointX,height,SEpointZ);
+    glTexCoord2f(0.0,100.0);
+    glVertex3f(NWpointX,height,SEpointZ);
+
+    glEnd();
+    glPopMatrix();
+}
+
+
+
+void createPlane(GLuint texture){
+     glBindTexture( GL_TEXTURE_2D, texture );
+     glColor4f(0,0,.5,0.6);
+    glPushMatrix();
+    glColor4f(0,1,0,0.7);
+   // glTranslatef(0,-2.5,0);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0);
+    glVertex3f(-10,0,10);
+    glTexCoord2f(10,0);
+    glVertex3f(10,0,10);
+    glTexCoord2f(10,10);
+    glVertex3f(10,0,-10);
+    glTexCoord2f(0,10);
+    glVertex3f(-10,0,-10);
+    glEnd();
+    glPopMatrix();
+}
+
+void createPool(){
+    //glBindTexture( GL_TEXTURE_2D, texture );
+    glColor4f(0,0,1,0.6);
+    glPushMatrix();
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0);
+    glVertex3f(4,.01,4);
+    glTexCoord2f(1,0);
+    glVertex3f(7,.01,4);
+    glTexCoord2f(1,1);
+    glVertex3f(7,.01,-4);
+    glTexCoord2f(0,1);
+    glVertex3f(4,.01,-4);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0);
+    glVertex3f(-4,.01,-2);
+    glTexCoord2f(1,0);
+    glVertex3f(-2,.01,-2);
+    glTexCoord2f(1,1);
+    glVertex3f(-2,.01,-4);
+    glTexCoord2f(0,1);
+    glVertex3f(-4,.01,-4);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0);
+    glVertex3f(-4,.01,8);
+    glTexCoord2f(1,0);
+    glVertex3f(-2,.01,8);
+    glTexCoord2f(1,1);
+    glVertex3f(-2,.01,6);
+    glTexCoord2f(0,1);
+    glVertex3f(-4,.01,6);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0,0);
+    glVertex3f(-7,.01,5);
+    glTexCoord2f(1,0);
+    glVertex3f(-5,.01,5);
+    glTexCoord2f(1,1);
+    glVertex3f(-5,.01,3);
+    glTexCoord2f(0,1);
+    glVertex3f(-7,.01,3);
+    glEnd();
+    glPopMatrix();
+}
+
+
+void reflect1(){
+    glClearStencil(0); //clear the stencil buffer
+    glClearDepth(1.0f);
+    glClearColor (0.0,0.0,0.0,1);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); //clear the buffers
+    glLoadIdentity();
+
+    glTranslatef(0, 0, -10);
+//start
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); //disable the color mask
+    glDepthMask(GL_FALSE); //disable the depth mask
+
+    glEnable(GL_STENCIL_TEST); //enable the stencil testing
+
+    glStencilFunc(GL_ALWAYS, 1, 0xFFFFFFFF);
+    glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE); //set the stencil buffer to replace our next lot of data
+
+}
+
+void reflect2(){
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE); //enable the color mask
+    glDepthMask(GL_TRUE); //enable the depth mask
+
+    glStencilFunc(GL_EQUAL, 1, 0xFFFFFFFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP); //set the stencil buffer to keep our next lot of data
+
+    glDisable(GL_DEPTH_TEST); //disable depth testing of the reflection
+    glPushMatrix();
+
+    //flip the reflection vertically
+    glTranslatef(0,0,0); //translate the reflection onto the drawing plane
+    glScalef(1.0f, -1.0f, 1.0f);
+}
+
---
+++ shad-replect/main.cpp
@@ -0,0 +1,261 @@
+
+//#include <windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "header.h"
+
+static float rot = 0;
+static float x1 = 0;
+static float change = 0.2;
+static float carMov = 0.2;
+static float yAxis;
+static float xAxis;
+static float pointX;
+static float pointY;
+static float sc = 1;
+GLfloat cube_color[4] = {1,0,0,1};
+GLuint texture1;
+GLuint texture2;
+GLuint texture3;
+GLuint texture4;
+GLuint texture5;
+GLuint water;
+GLuint asphalt;
+GLuint grass;
+
+//car coordinates
+float xCoord = 0;
+float zCoord = 10;
+
+void controlRotate(unsigned char key,int x,int y){
+
+    switch(key){
+
+        case 'w':
+            yAxis+=change;
+           // printf("moving");
+            break;
+        case 's':
+            yAxis-=change;
+           // printf("moving");
+            break;
+        case 'q':
+            xAxis -= change;
+            break;
+        case 'e':
+            xAxis += change;
+            break;
+        case 'd':
+            rot += change;
+            break;
+        case 'a':
+            rot -= change;
+            break;
+
+        case 'p':
+            sc +=change/10;
+            break;
+        case 'o':
+            sc -=change/10;
+            break;
+        //changes in lighting
+
+    }
+
+}
+void applyMat(float R, float G, float B){
+    GLfloat cube_color[4] = {R, G, B,1};
+    glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT, cube_color);
+}
+void rotate(){
+
+    zCoord-=carMov/2;
+    if(zCoord < 0){
+        carMov -=carMov/50;
+    }
+    glutPostRedisplay();
+}
+
+//maybe a function to create the town
+void display()
+{
+     glEnable( GL_TEXTURE_2D );
+     //glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_COMPONENTS, &value);
+
+    texture1 = LoadTexture( "building.raw", 256, 256 );
+    texture2 = LoadTexture("texture3.raw",256,256);
+    texture3 = LoadTexture( "building3.raw", 256, 256 );
+    texture4 = LoadTexture( "texture5.raw", 256, 256 );
+    texture5 = LoadTexture( "texture6.raw", 256, 256 );
+
+    asphalt = LoadTexture( "asphalt.raw", 256, 256 );
+    water = LoadTexture( "water.raw", 256, 256 );
+    grass = LoadTexture( "grass.raw", 256, 256 );
+
+    reflect1();
+
+    gluLookAt(0,yAxis,-5,0,0,0,0,1,0);
+    glTranslatef(xAxis,0,0);
+    glScalef(sc, sc, sc);
+    glRotatef(rot*2,0,1,0);
+
+    createPool();
+
+    reflect2();
+
+    //reflected buildings start here
+    createBuilding(-1,0,-2,2,6, texture5);
+    createBuilding(2,1,0,0,3, texture3);
+    createBuilding(0,-1,1,-2,2, texture1);
+    //world trade
+    createBuilding(-2,3,-4,5,10,texture1);
+    createBuilding(-7,8,-5,6,10,texture1);
+
+    createBuilding(-2,-2,-1,-1,6, texture3);
+    createBuilding(2,-1,3,-3,6.5,texture2);
+    createBuilding(3,-1,4,-2,4,texture3);
+    createBuilding(7,-2,9,-5,4,texture5);
+    createBuilding(0,3,1,5,6,texture1);
+    createBuilding(1,4,2,5,3,texture5);
+    createBuilding(2,3,3,5,6,texture1);
+    createBuilding(-2,-4,-1,-5,2,texture4);
+    createBuilding(0,-4,2,-5,1,texture2);
+    createBuilding(-5,-1,-3,-2,8, texture4);
+    createBuilding(0,8,6,6,2,texture1);
+    createBuilding(8,3,9,-1,1,texture3);
+    createBuilding(-4,2,-3,0,5,texture2);
+    createBuilding(-9,-3,-10,-1,6,texture3);
+    createBuilding(-7,-4,-8,-1,5,texture2);
+    createBuilding(-5,-9,-4,-10,4,texture4);
+    createBuilding(1,-8,6,-10,2,texture3);
+    createBuilding(6,-8,8,-9,1,texture4);
+    createBuilding(8,-8,9,-10,2,texture3);
+    createBuilding(-9,-7,-7,-9,5,texture5);
+    createBuilding(-9,2,-5,1,5,texture1);
+    createBuilding(-8,1,-6,0,2,texture3);
+    createBuilding(-9,5,-8,3,4,texture3);
+    createBuilding(7,8,9,6,4,texture5);
+
+
+    glPopMatrix();
+
+    glEnable(GL_DEPTH_TEST); //enable the depth testing
+    glDisable(GL_STENCIL_TEST); //disable the stencil testing
+    glEnable(GL_BLEND); //enable alpha blending
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //set the blending function
+
+    createPool();
+
+    glDisable(GL_BLEND); //disable alpha blending
+    createPlane(grass);
+    glEnable(GL_LIGHTING);
+
+    //actual buildings start here
+    createBuilding(-1,0,-2,2,6, texture5);
+    createBuilding(2,1,0,0,3, texture3);
+    createBuilding(0,-1,1,-2,2, texture1);
+    //world trade
+    createBuilding(-2,3,-4,5,10,texture1);
+    createBuilding(-7,8,-5,6,10,texture1);
+
+    createBuilding(-2,-2,-1,-1,6, texture3);
+    createBuilding(2,-1,3,-3,6.5,texture2);
+    createBuilding(3,-1,4,-2,4,texture3);
+    createBuilding(7,-2,9,-5,4,texture5);
+    createBuilding(0,3,1,5,6,texture1);
+    createBuilding(1,4,2,5,3,texture5);
+    createBuilding(2,3,3,5,6,texture1);
+    createBuilding(-2,-4,-1,-5,2,texture4);
+    createBuilding(0,-4,2,-5,1,texture2);
+    createBuilding(-5,-1,-3,-2,8, texture4);
+    createBuilding(0,8,6,6,2,texture1);
+    createBuilding(8,3,9,-1,1,texture3);
+    createBuilding(-4,2,-3,0,5,texture2);
+    createBuilding(-9,-3,-10,-1,6,texture3);
+    createBuilding(-7,-4,-8,-1,5,texture2);
+    createBuilding(-5,-9,-4,-10,4,texture4);
+    createBuilding(1,-8,6,-10,2,texture3);
+    createBuilding(6,-8,8,-9,1,texture4);
+    createBuilding(8,-8,9,-10,2,texture3);
+    createBuilding(-9,-7,-7,-9,5,texture5);
+    createBuilding(-9,2,-5,1,5,texture1);
+    createBuilding(-8,1,-6,0,2,texture3);
+    createBuilding(-9,5,-8,3,4,texture3);
+    createBuilding(7,8,9,6,4,texture5);
+
+    //car
+    createCar(xCoord,zCoord,xCoord-.3,zCoord+.5,.5);
+    createCar(xCoord-.6,zCoord+1,xCoord-.3, zCoord+1.5,.5);
+
+    //streets
+    createStreet(-1,10,0,-10,asphalt);
+    createStreet(-10,0,4,-1,asphalt);
+    createStreet(-2,10,-1,2,asphalt);
+    createStreet(-10,2,4,3,asphalt);
+    createStreet(3,0,4,2,asphalt);
+    createStreet(-10,-5,10,-7,asphalt);
+    createStreet(-10,6,10,5,asphalt);
+    createStreet(-5,10,-4,0,asphalt);
+    createStreet(9,5,10,-5,asphalt);
+    createStreet(-6,-1,-5,-10,asphalt);
+    createStreet(0,9,7,8,asphalt);
+    createStreet(6,8,7,6,asphalt);
+    createStreet(-8,9,0,8,asphalt);
+    createStreet(-9,9,-7,3,asphalt);
+
+    glDisable(GL_LIGHTING);
+     FreeTexture(texture1);
+     FreeTexture(texture2);
+     FreeTexture(texture3);
+     FreeTexture(texture4);
+     FreeTexture(texture5);
+     FreeTexture(asphalt);
+     FreeTexture(water);
+     FreeTexture(grass);
+
+   glutSwapBuffers();
+}
+
+
+void init()
+{
+
+    GLfloat light_pos[4] = { 13,15,15,0};
+    GLfloat ambient_color[4] = { .8,.8,.8,1};
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glShadeModel (GL_SMOOTH);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluPerspective (60, 1, 0.1, 1000.0);
+    glMatrixMode (GL_MODELVIEW);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+    glLightfv(GL_LIGHT0,GL_AMBIENT, ambient_color);
+
+  glutKeyboardFunc(controlRotate);
+}
+
+int main(int argc,char ** argv)
+{
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
+  glutInitWindowSize(500,500);
+  glutCreateWindow("simple");
+  glutIdleFunc(rotate);
+  glutDisplayFunc(display);
+  init();
+
+  glutMainLoop();
+  return 0;
+}
